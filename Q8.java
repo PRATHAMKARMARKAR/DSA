de10da4218bcce214efed15fdf9a51d6cfc3d52c
PRATHAM KARMARKAR <137695598+PRATHAMKARMARKAR@users.noreply.github.com>
2024-06-06 09:38:02 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+         System.out.println("ENTER THE LINES OF PATTERN");
+         int n = s.nextInt();
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+        
+                for (int j = 0; j < 2 * n - 2*i-1; j++) {
+                    System.out.print("*");
+                
+                }
+           
+            System.out.println();
+         }
+    }
+}
