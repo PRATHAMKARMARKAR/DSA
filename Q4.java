10638ed2ec7e78337d8c261e1f48a854cb4f280c
PRATHAM KARMARKAR <137695598+PRATHAMKARMARKAR@users.noreply.github.com>
2024-07-11 21:43:34 +0530

    reverse array

--- Q4.java
+++ Q4.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
+package Recursion;
 
 public class Q4 {
-    public static void main(String[] args) {
-     Scanner s = new Scanner(System.in);
-        System.out.println("ENTER THE NUMBER OF LINES -->");
-        int n = s.nextInt();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i);
-            }System.out.println();
-    }
-}
+    static void printArray(int ans[], int n) {
+        System.out.print("Reversed array is:- \n");
+        for (int i = 0; i < n; i++) {
+           System.out.print(ans[i] + " ");
+        }
+     }
+     //Function to reverse array using an auxiliary array
+     static void reverseArray(int arr[], int n) {
+        int[] ans = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+           ans[n - i - 1] = arr[i];
+        }
+        printArray(ans, n);
+     }
+     public static void main(String[] args) {
+        int n = 5;
+        int arr[] = { 5, 4, 3, 2, 1};
+        reverseArray(arr, n);
+     }
 }
