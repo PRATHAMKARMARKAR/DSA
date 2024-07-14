33d6eed37ed451e03ea2b3c1df9e0e21fdec0ba2
PRATHAM KARMARKAR <137695598+PRATHAMKARMARKAR@users.noreply.github.com>
2024-07-14 19:14:15 +0530

    sum of number

--- Q5.java
+++ Q5.java
@@ -1,14 +1,16 @@
-import java.util.Scanner;
+package Recursion;
 
 public class Q5 {
     public static void main(String[] args) {
-         Scanner s = new Scanner(System.in);
-        System.out.println("ENTER THE NUMBER OF LINES -->");
-        int n = s.nextInt();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++) {
-                 System.out.print(j);
-            }System.out.println();
-    }
-}
+
+        solve(5);
+        solve(6);
+      }
+      public static void solve(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+          sum += i;
+        }
+        System.out.println("The sum of the first " + n + " numbers is: " + sum);
+      }
 }
