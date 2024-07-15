6ef41e7a6991b6575aa692c46999f5bd4ee66615
PRATHAM KARMARKAR <137695598+PRATHAMKARMARKAR@users.noreply.github.com>
2024-07-15 21:48:17 +0530

    Q6 in recursion aded

--- Q6.java
+++ Q6.java
@@ -1,21 +1,17 @@
-import java.util.Scanner;
+package Recursion;
 
 public class Q6 {
-    public static void main(String[] args) {
-         Scanner s = new Scanner(System.in);
-        
-        System.out.println("ENTER THE NUMBER OF LINES");
-        int n = s.nextInt();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < n-i; j++) {
-                System.out.print(" ");
-            }
-            for(int j=0;j< i*2-1;j++){
-                System.out.print("*");
-                
-            }
-            System.out.println();
+    static int factorial(int X) {
+        int ans = 1;
+        for (int i = 1; i <= X; i++) {
+           ans = ans * i;
         }
-        
-    }
+        return ans;
+     }
+     public static void main(String[] args) {
+  
+        int X = 5;
+        int result = factorial(X);
+        System.out.println("The factorial of " + X + " is " + result);
+     }
 }
