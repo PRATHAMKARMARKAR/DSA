7b60abab13f0ace6d9bdaf6b9535bf51d7f03a5b
PRATHAM KARMARKAR <137695598+PRATHAMKARMARKAR@users.noreply.github.com>
2024-07-22 23:47:31 +0530

    q9

--- Q9.java
+++ Q9.java
@@ -1,61 +1,19 @@
+package Recursion;
+
 public class Q9 {
-    // Method to print an erect pyramid
-    static void erect_pyramid(int N) {
-        // Outer loop for rows
-        for (int i = 0; i < N; i++) {
-            // Loop to print spaces before stars in each row
-            for (int j = 0; j < N - i - 1; j++) {
-                System.out.print(" ");
-            }
-
-            // Loop to print stars in each row
-            for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print("*");
-            }
-
-            // Loop to print spaces after stars in each row
-            for (int j = 0; j < N - i - 1; j++) {
-                System.out.print(" ");
-            }
-
-            // Move to the next line after printing stars for each row
-            System.out.println();
-        }
-    }
-
-    // Method to print an inverted pyramid
-    static void inverted_pyramid(int N) {
-        // Outer loop for rows
-        for (int i = 0; i < N; i++) {
-            // Loop to print spaces before stars in each row
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-
-            // Loop to print stars in each row
-            for (int j = 0; j < 2 * N - (2 * i + 1); j++) {
-                System.out.print("*");
-            }
-
-            // Loop to print spaces after stars in each row
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-
-            // Move to the next line after printing stars for each row
-            System.out.println();
-        }
-    }
-
-    // Main method
-    public static void main(String[] args) {
-        // Value of N
-        int N = 5;
-        
-        // Print erect pyramid
-        erect_pyramid(N);
-        
-        // Print inverted pyramid
-        inverted_pyramid(N);
+    int n = 5;
+    if (n == 0) {
+      System.out.println(0);
+    } else {
+      int fib[] = new int[n + 1];
+      fib[0] = 0;
+      fib[1] = 1;
+      for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+      }
+      System.out.println("The Fibonacci Series up to "+n+"th term:");
+      for (int i = 0; i <= n; i++) {
+        System.out.print(fib[i] + " ");
+      }
     }
 }
